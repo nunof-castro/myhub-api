ac2f77e03f1110a735aefe893f148cea80a9ffa1
nunof-castro <nunocos125@gmail.com>
2023-04-25 20:32:44 +0100

    establish database connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
+import express from "express";
 import { AppDataSource } from "./data-source";
 
 AppDataSource.initialize()
-  .then(async () => {})
+  .then(async () => {
+    const serverPort = process.env.SERVER_PORT;
+
+    const app = express();
+    app.use(express.json());
+
+    app.get("/", (req, res) => {
+      return res.json(`MY HUB API`);
+    });
+
+    return app.listen(serverPort, () =>
+      console.log(`Server running on port ${serverPort}`)
+    );
+  })
   .catch((error) => console.log(error));
