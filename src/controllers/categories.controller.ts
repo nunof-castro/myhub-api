9ded8d9df5bc19fed12d63d9a64012c46ff247f6
nunof-castro <nunocos125@gmail.com>
2023-04-28 15:11:22 +0100

    • create a general subcategory • delete subcategories when category is deleted • get all categories error handling

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -2,8 +2,10 @@ import { Request, Response } from "express";
 import { ILike } from "typeorm";
 import { AppDataSource } from "../data-source";
 import { Category } from "../entities/Category";
+import { SubCategory } from "../entities/SubCategory";
 
 const categoryRepository = AppDataSource.getRepository(Category);
+const subCategoryRepository = AppDataSource.getRepository(SubCategory);
 
 export const createCategory = async (req: Request, res: Response) => {
   const { name } = req.body;
@@ -22,8 +24,14 @@ export const createCategory = async (req: Request, res: Response) => {
     const newCategory = new Category();
     newCategory.name = name;
 
+    const newSubCategory = new SubCategory();
+    newSubCategory.name = name;
+    newSubCategory.category = newCategory;
+
     try {
       await categoryRepository.save(newCategory);
+      await subCategoryRepository.save(newSubCategory);
+
       return res.status(201).json({
         category: newCategory,
         message: "Category created with success",
@@ -36,7 +44,13 @@ export const createCategory = async (req: Request, res: Response) => {
 
 export const getCategories = async (req: Request, res: Response) => {
   try {
-    return res.status(200).json(await categoryRepository.find());
+    const categories = await categoryRepository.find();
+
+    if (categories.length !== 0) {
+      return res.status(200).send(categories);
+    } else {
+      return res.status(400).send("No categories created");
+    }
   } catch (error) {
     return res.status(500).json({ message: "Failed to get categories" });
   }
@@ -52,7 +66,17 @@ export const deleteCategory = async (req: Request, res: Response) => {
 
     const { name } = categoryToDelete;
 
+    // delete all subcategories associated with the category
+    await subCategoryRepository
+      .createQueryBuilder()
+      .delete()
+      .from(SubCategory)
+      .where("category_id = :id", { id: Number(id) })
+      .execute();
+
+    // delete the category
     await categoryRepository.remove(categoryToDelete);
+
     return res
       .status(200)
       .json({ message: `Category ${name} deleted successfully` });
